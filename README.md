d498ee35800d2d1cd019afe835a8bfdf4c9d12f1
Twan Goosen <twan@clarin.eu>
2025-09-17 16:12:14 +0300

    Command line options for the run script. Added a README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Add Wikidata concepts to VLO configuration
+
+
+## Usage
+
+Usage:
+```sh
+bash run.sh \
+	-i /my/vlo/config/facetConcepts.xml \
+	-m /my/wikidata/mapping.csv \
+	-o /my/vlo/newConfig/facetConcepts.xml
+```
--- run.sh
+++ run.sh
@@ -1,5 +1,59 @@
 #!/usr/bin/env bash
-(
-	cd "$(dirname $0)/docker" \
-	&& docker-compose run --rm 'vlo-config-wikidata'
-)
+set -e
+
+BASEDIR="$(cd "$(dirname $0)" && pwd)"
+
+FACET_CONFIG_FILE="${BASEDIR}/test/facetConcepts.xml"
+MAPPING_CSV="${BASEDIR}/test/test.csv"
+OUT_DIR="${BASEDIR}/output"
+OUT_FILE="facetConcepts.xml"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -i)
+    	FACET_CONFIG_FILE="$(realpath "$2")"
+    	shift
+    	shift
+    	;;
+    -m)
+    	MAPPING_CSV="$(realpath "$2")"
+    	shift
+    	shift
+    	;;
+    -o)
+    	OUT_DIR="$(realpath "$(dirname $2)")"
+    	OUT_FILE="$(basename $2)"
+    	shift
+    	shift
+    	;;
+  esac
+done
+
+if ! [ -e "${FACET_CONFIG_FILE}" ]; then
+	echo "Error - no such file: ${FACET_CONFIG_FILE} "
+	exit 1
+fi
+
+if ! [ -e "${MAPPING_CSV}" ]; then
+	echo "Error - no such file: ${MAPPING_CSV} "
+	exit 1
+fi
+
+if ! [ -d "${OUT_DIR}" ]; then
+	echo "Creating output directory: ${OUT_DIR}"
+	mkdir -p "${OUT_DIR}"
+fi
+
+
+export MAPPING_INFILE="${MAPPING_CSV}"
+export FACET_MAPPING_INFILE="${FACET_CONFIG_FILE}"
+export OUTPUT_DIR="${OUT_DIR}"
+export OUTPUT_FILE="${OUT_FILE}"
+
+echo "Concepts wikidata mapping CSV: ${MAPPING_INFILE}"
+echo "Facet concept mapping configuration input file: ${FACET_MAPPING_INFILE}"
+echo "Output will be written to ${OUTPUT_DIR}/${OUTPUT_FILE}"
+
+
+( cd "${BASEDIR}/docker" && docker-compose run --rm 'vlo-config-wikidata' )
+
